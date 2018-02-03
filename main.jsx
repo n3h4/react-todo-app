d8cdadaee5c85006564814a192db09b03497eb90
n3h4 <nehashaik301@gmail.com>
2018-02-03 16:08:07 +0530

    modify

--- main.jsx
+++ main.jsx
@@ -1,21 +1,16 @@
 
 class TodoApp extends React.Component {
+
     constructor() {
         super();
-        this.state = { text: '', items: [] };
-    }
+        this.state = { text: '', todos: [] };
 
-    handleDelete(itemToBeDeleted) {
-
-        // console.log(itemToBeDeleted);  
-        var newItems = this.state.items.filter((item) => {
-            return item != itemToBeDeleted
-        });
-        this.setState({ items: newItems });
     }
 
     handleChange(e) {
+
         this.setState({ text: e.target.value });
+
     }
 
     handleSubmit(e) {
@@ -23,14 +18,39 @@ class TodoApp extends React.Component {
         e.preventDefault();
 
         var prevItem = { text: this.state.text };
-        var newItems = this.state.items.concat(prevItem);
+        var newTodos = this.state.todos.concat(prevItem);
 
-        this.setState({ text: '', items: newItems });
+        this.setState({ text: '', todos: newTodos });
+    }
 
+    handleDelete(itemToBeDeleted) {
+
+        // console.log(itemToBeDeleted);  
+        var newTodos = this.state.todos.filter((todo) => {
+            return todo != itemToBeDeleted
+        });
+
+        this.setState({ todos: newTodos });
+    }
+
+    handleDone(markAsDone) {
+
+        var _todo = this.state.todos;
+
+        var newTodo = _todo.filter((todo) => {
+
+            return todo === markAsDone;
+
+        })[0];
+        // console.log("working");
+
+        done != done;
+        this.setState({ todos: newTodo })
     }
 
     render() {
         return (
+
             <div>
                 <h1 className="header">Todo-List</h1>
 
@@ -40,39 +60,67 @@ class TodoApp extends React.Component {
                         value={this.state.text} />
                 </form>
 
-                <TodoList handleDelete={this.handleDelete.bind(this)} items={this.state.items}
+                <div>
+                    <p>All: {this.state.todos.length}</p>
+                    <p>Completed: {this.state.todos.filter((todo) => { }).length}</p>
+                    <p>Pending: </p>
+                </div>
+
+                <TodoList handleDelete={this.handleDelete.bind(this)} todos={this.state.todos} handleDone={this.handleDone.bind(this)}
                 />
             </div>
         );
     }
-
 }
 
 class TodoList extends React.Component {
-    constructor() {
-        super();
-        this.state = { done: false};
-    }
-    handleOnChange(){
-        var done = !this.state.done;
-        this.setState({done: done});
-    }
+
     render() {
         return (
+
             <div>
 
                 <ul className="list">
-                    {this.props.items.map((item, index) => (
-                        <li key={index}><input type="checkbox" onChange={this.handleOnChange.bind(this)} checked={this.state.done}/>{item.text}
-                            <button className="delete" onClick={this.props.handleDelete.bind(this, item)}></button></li>
-                    ))}
+                    {this.props.todos.map((item, index) => {
+
+                        return <TodoItem
+
+                            handleDone={this.props.handleDone}
+                            handleDelete={this.props.handleDelete.bind(this)}
+
+                            item={item} />
+                    })}
+
                 </ul>
 
             </div>
         );
     }
+}
 
+class TodoItem extends React.Component {
 
+    constructor() {
+        super();
+        this.state = { done: false };
+    }
+
+    render() {
+        var item = this.props.item;
+
+        return (
+
+            <li key={item}>
+
+                <input type="checkbox"
+                    onChange={this.props.handleDone.bind(this)}
+                    checked={this.state.done}
+                />  {item.text}
+                <button className="delete" onClick={this.props.handleDelete.bind(this, item)}></button>
+            </li>
+
+        );
+    }
 }
 
 
